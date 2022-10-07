0abe1b2fef279a98f5a8e32dee3e5bb0747ad1de
Miguel Cemiller <miguelcemiller@gmail.com>
2022-10-07 11:26:36 +0800

    started stages slider

--- static/js/index.js
+++ static/js/index.js
@@ -99,6 +99,7 @@ const iconsListener = (icons) => {
         data.category = temp;
       }
 
+      console.log(temp);
       // Data Validation
       dataValidation();
     });
@@ -151,3 +152,92 @@ function previewImage() {
   // Data Validation
   dataValidation();
 }
+
+/* Slider */
+// Source: https://stackoverflow.com/questions/58353280/prevent-click-when-leave-drag-to-scroll-in-js/58353989#58353989
+// https://stackabuse.com/how-to-create-a-draggable-carousel-using-vanilla-javascript/
+const slider = document.querySelector(".slider");
+
+const preventClick = (e) => {
+  e.preventDefault();
+  e.stopImmediatePropagation();
+};
+
+let isDown = false;
+var isDragged = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+  slider.style.cursor = "grabbing";
+});
+slider.addEventListener("touchstart", (e) => {
+  isDown = true;
+  startX = e.changedTouches[0].pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+  slider.style.cursor = "grabbing";
+});
+
+slider.addEventListener("mouseenter", () => {
+  slider.style.cursor = "grab";
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.style.cursor = "default";
+});
+slider.addEventListener("touchleave", () => {
+  isDown = false;
+  slider.style.cursor = "default";
+});
+
+slider.addEventListener("mouseup", (e) => {
+  isDown = false;
+  const elements = document.getElementsByClassName("book");
+  if (isDragged) {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].addEventListener("click", preventClick);
+    }
+  } else {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].removeEventListener("click", preventClick);
+    }
+  }
+  isDragged = false;
+  slider.style.cursor = "grab";
+});
+slider.addEventListener("touchend", (e) => {
+  isDown = false;
+  const elements = document.getElementsByClassName("stage-icon-container");
+  if (isDragged) {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].addEventListener("click", preventClick);
+    }
+  } else {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].removeEventListener("click", preventClick);
+    }
+  }
+  isDragged = false;
+  slider.style.cursor = "grab";
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  isDragged = true;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+});
+slider.addEventListener("touchmove", (e) => {
+  if (!isDown) return;
+  isDragged = true;
+  e.preventDefault();
+  const x = e.changedTouches[0].pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+});
